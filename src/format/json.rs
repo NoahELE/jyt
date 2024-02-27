f47902420e44bb2d66c34e783049589f5ce15ff8
NoahELE <cxhmel@outlook.com>
2024-02-28 10:46:18 +1100

    refactor: modify code for readability

--- src/format/json.rs
+++ src/format/json.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 use super::error::{DeError, SerError};
 
 pub fn serialize<V: Serialize>(v: V) -> Result<String, SerError> {
-    serde_json::to_string_pretty(&v).map_err(Into::into)
+    serde_json::to_string_pretty(&v).map_err(|e| e.into())
 }
 
 pub fn deserialize<V>(s: &str) -> Result<V, DeError>
 where
     V: for<'de> Deserialize<'de>,
 {
-    serde_json::from_str(s).map_err(Into::into)
+    serde_json::from_str(s).map_err(|e| e.into())
 }
--- src/format/yaml.rs
+++ src/format/yaml.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 use super::error::{DeError, SerError};
 
 pub fn serialize<V: Serialize>(v: V) -> Result<String, SerError> {
-    serde_yaml::to_string(&v).map_err(Into::into)
+    serde_yaml::to_string(&v).map_err(|e| e.into())
 }
 
 pub fn deserialize<V>(s: &str) -> Result<V, DeError>
 where
     V: for<'de> Deserialize<'de>,
 {
-    serde_yaml::from_str(s).map_err(Into::into)
+    serde_yaml::from_str(s).map_err(|e| e.into())
 }
