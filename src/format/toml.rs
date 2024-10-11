b9bd262a40d50242e62bc8e0a133183b48067ff1
NoahELE <49080986+NoahELE@users.noreply.github.com>
2024-10-11 23:28:45 +1100

    refactor: improve readability

--- src/format/toml.rs
+++ src/format/toml.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 use super::error::{DeError, SerError};
 
 pub fn serialize<V: Serialize>(v: V) -> Result<String, SerError> {
-    toml::to_string_pretty(&v).map_err(|e| e.into())
+    toml::to_string_pretty(&v).map_err(SerError::from)
 }
 
 pub fn deserialize<V>(s: &str) -> Result<V, DeError>
 where
     V: for<'de> Deserialize<'de>,
 {
-    toml::from_str(s).map_err(|e| e.into())
+    toml::from_str(s).map_err(DeError::from)
 }
--- src/format/yaml.rs
+++ src/format/yaml.rs
@@ -4,12 +4,12 @@ use serde::{Deserialize, Serialize};
 use super::error::{DeError, SerError};
 
 pub fn serialize<V: Serialize>(v: V) -> Result<String, SerError> {
-    serde_yaml::to_string(&v).map_err(|e| e.into())
+    serde_yaml::to_string(&v).map_err(SerError::from)
 }
 
 pub fn deserialize<V>(s: &str) -> Result<V, DeError>
 where
     V: for<'de> Deserialize<'de>,
 {
-    serde_yaml::from_str(s).map_err(|e| e.into())
+    serde_yaml::from_str(s).map_err(DeError::from)
 }
